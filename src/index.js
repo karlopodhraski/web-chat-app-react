cb5c3c4861909a33228c7138aa1bd24186c23b59
karlopodhraski <133400296+karlopodhraski@users.noreply.github.com>
2023-07-26 17:37:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorker.unregister();
 
 
-reportWebVitals();
+
